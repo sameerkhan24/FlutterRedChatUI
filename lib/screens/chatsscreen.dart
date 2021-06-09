947437ee62aaa7138344d261a06f6e40cd1a10cb
Sameer Khan <sameerkhan241@outlook.com>
2021-06-09 06:13:10 +0530

    Added Chat Messages

--- lib/screens/chatsscreen.dart
+++ lib/screens/chatsscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_chat_ui_starter/models/message_model.dart';
 import 'package:flutter_chat_ui_starter/models/user_model.dart';
 
@@ -13,6 +14,50 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+
+  _buildMessage(Message message, bool isMe){
+    return Container(
+      margin: isMe 
+      ? EdgeInsets.only(top: 8,bottom: 8,left: 80)
+      : EdgeInsets.only(top: 8,bottom: 8,right: 80),
+      padding: EdgeInsets.symmetric(horizontal: 25,vertical: 15),
+      decoration: BoxDecoration(
+        color: isMe ? Theme.of(context).accentColor :Color(0xFFFFEFEE),
+        borderRadius: isMe? BorderRadius.only(
+          topLeft: Radius.circular(15),
+          bottomLeft: Radius.circular(15)
+        )
+        :BorderRadius.only(
+          topRight: Radius.circular(15),
+          bottomRight: Radius.circular(15)
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            message.time,
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 16,
+              fontWeight: FontWeight.bold
+            ),
+            ),
+          SizedBox(height: 8,),
+          Text(
+            message.text,
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 16,
+              fontWeight: FontWeight.bold
+            ),
+            ),
+        ],
+      ),
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,11 +97,13 @@ class _ChatScreenState extends State<ChatScreen> {
                   topRight: Radius.circular(30),
                 ),
                 child: ListView.builder(
+                  reverse: true,
                   itemCount: messages.length,
                   padding: EdgeInsets.only(top: 15),
                   itemBuilder: (BuildContext context, int index){
                     final Message message = messages[index];
-                    return Text(message.text);
+                    final bool isMe = message.sender.id == currentUser.id;
+                    return _buildMessage(message, isMe);
                   },
                 ),
               ),
